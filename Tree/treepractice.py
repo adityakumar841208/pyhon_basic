18e6788121c441cdc09b5dc2a48c42394c6d66bf
Aditya <adityakumar8340203184@gmail.com>
2024-12-05 09:49:07 +0530

    tree delete function added

--- Tree/treepractice.py
+++ Tree/treepractice.py
@@ -19,6 +19,34 @@ class Tree:
             root.right = self.insert(root.right, key)
 
         return root
+    
+    def delete(self,root, key):
+        if root == None:
+            return root
+    
+
+        if key < root.key:
+            root.left = self.delete(root.left,key)
+        elif key > root.key:
+            root.right = self.delete(root.right,key)
+
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+        
+            # go to the smallest number at the root right and get it's data and make this to the root data
+            temp = self.findMinRoot(root.right)
+            root.key = temp.key
+            root.right = self.delete(root.right,temp.key)
+        return root
+
+    def findMinRoot(self,root):
+        while root.left:
+            root = root.left
+        return root
+
 
     def inorder(self, root):
         if root:
@@ -34,4 +62,6 @@ tree = Tree()
 for data in a:
     tree.root = tree.insert(tree.root, data)
 
+tree.root = tree.delete(tree.root,2)
+tree.root = tree.delete(tree.root,7)
 tree.inorder(tree.root)
