079aa5af685b61c546364df08b769be519b23e08
Aditya <adityakumar8340203184@gmail.com>
2024-12-10 18:14:58 +0530

    minor changes

--- Tree/tree2.py
+++ Tree/tree2.py
@@ -31,7 +31,7 @@ tree = Tree()
 tree.root = tree.insert(tree.root,5)
 tree.root = tree.insert(tree.root,3)
 tree.root = tree.insert(tree.root,6)
-tree.root = tree.
+tree.root = tree.insert(tree.root,9)
 
 tree.traversal(tree.root)
 
